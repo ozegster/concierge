fb037f9573b32bba235d7c5b22403f87dc52aae7
nikola <nikola.lisicic@hotmail.com>
2017-03-21 22:10:23 +0100

    Change build files

---
+++ con-master/src/main/java/ba/codecentric/MasterMain.java
@@ -0,0 +1,14 @@
+package ba.codecentric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Nikola on 21.3.2017..
+ */
+@SpringBootApplication
+public class MasterMain {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MasterMain.class, args);
+    }
+}
