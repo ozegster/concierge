378513eb83c9ec92c02fa554b4af10d3bb0f49a6
DraganKec <Omen1983>
2017-03-23 20:14:52 +0100

    CON-9 - Create sql script form concierge database

---
+++ con-base/src/scripts/db/concierge_database.sql
@@ -0,0 +1,15 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema concierge
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `concierge` DEFAULT CHARACTER SET utf8mb4 ;
+USE `concierge` ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
