0055804ecc639d27734109a34541d9f4b02293a8
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-03-28 21:28:53 +0200

    Merge branch 'integration' of C:\Users\Gosa\IdeaProjects\concierge with conflicts.

---
+++ con-base/src/main/java/ba/codecentric/base/service/HotelServiceImpl.java
@@ -0,0 +1,24 @@
+package ba.codecentric.base.service;
+
+import ba.codecentric.base.domain.Hotel;
+import ba.codecentric.base.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class HotelServiceImpl implements HotelService {
+
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    public HotelServiceImpl(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    @Override
+    public void saveHotel(Hotel hotel) {
+        hotelRepository.save(hotel);
+    }
+
+
+}
+CON-12
