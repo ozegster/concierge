8e192b82adfb2aefac3a1465f7f82d68ca7f783d
nikola <nikola.lisicic@hotmail.com>
2017-03-28 21:11:13 +0200

    CON-7 - Add CountryController

---
+++ con-base/src/main/java/ba/codecentric/base/controller/CountryController.java
@@ -0,0 +1,28 @@
+package ba.codecentric.base.controller;
+
+import ba.codecentric.base.domain.Country;
+import ba.codecentric.base.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    private CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @RequestMapping(value = "/countries", method = RequestMethod.GET)
+    public ResponseEntity<List<Country>> getCountries() {
+        return new ResponseEntity<List<Country>>(countryService.getAllCountries(), HttpStatus.OK);
+    }
+}
