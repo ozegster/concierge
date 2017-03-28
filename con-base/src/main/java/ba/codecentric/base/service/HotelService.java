fce0d29a75072fdfba7178cdf615f2fc8ff72b2a
BojanJakic <bojanjakic1@gmail.com>
2017-03-28 16:30:49 +0200

    Annotate iterface with @Service

--- con-base/src/main/java/ba/codecentric/base/service/HotelService.java
+++ con-base/src/main/java/ba/codecentric/base/service/HotelService.java
@@ -1,9 +1,11 @@
 package ba.codecentric.base.service;
 
-
+import org.springframework.stereotype.Service;
 import ba.codecentric.base.domain.Hotel;
 
+@Service
 public interface HotelService {
 
     void saveHotel(Hotel hotel);
+
 }
