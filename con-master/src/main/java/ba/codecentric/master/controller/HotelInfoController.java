77632eeea99174bd0ea711f09ba3263c293b3513
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-03-29 10:39:12 +0200

    CON-12 Create rest controller HotelInfoController

---
+++ con-master/src/main/java/ba/codecentric/master/controller/HotelInfoController.java
@@ -0,0 +1,21 @@
+package ba.codecentric.master.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelInfoController {
+
+    @Autowired
+    public HotelInfoController(HotelService hotelService){
+        this.hotelService = hotelService;
+    }
+
+    @RequestMapping("/savehotelinfo", method = RequestMethod.POST)
+    public void saveHotelInfo(@RequestBody HotelInfo hotelInfo) {
+        hotelService.saveHotel(hotel);
+    }
+}
