f01330302a715b13c2ba0a07c4324bc20e6a6e44
nikola <nikola.lisicic@hotmail.com>
2017-03-29 13:52:36 +0200

    CON-7 - Change the scope of hotel object in hotel-info.html
    Removed the hotel model from controller and put it in the $scope.
    Placed submit function in $scope also.

--- dashboard-app/src/app/pages/hotel-info/controllers/hotel-info.controller.js
+++ dashboard-app/src/app/pages/hotel-info/controllers/hotel-info.controller.js
@@ -1,18 +1,30 @@
 (function () {
     angular.module('ConciergeApp.pages.hotelinfo')
-        .controller('HotelInfoCtrl', ['HotelInfoService', function (HotelInfoService) {
-            var self = this;
+        .controller('HotelInfoCtrl', ['HotelInfoService','$scope', function (HotelInfoService,$scope) {
+            $scope.hotel = {
+                name: '',
+                rating: '',
+                address: '',
+                zip: '',
+                city: '',
+                country: {},
+                phone: '',
+                fax: '',
+                email: '',
+                website: '',
+                description: ''
+            }
 
             // getting countries from database to show in select box
             HotelInfoService.getCountries().then(function (data) {
-                self.countries = data;
+                $scope.countries = data;
             }, function (errorMessage) {
                 console.log(errorMessage);
             });
 
             // TO DO
-            self.submit = function () {
-                console.log(self.hotel);
+            $scope.submit = function () {
+                console.log($scope.hotel);
             }
         }])
 })();
