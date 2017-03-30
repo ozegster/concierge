073b003218d9352e3152d205b99c6b8f38181c75
nikola <nikola.lisicic@hotmail.com>
2017-03-30 19:38:25 +0200

    CON-7 - Implement CORS with a filter
    Added CORSFilter class which enables CORS from origin specified in application.properties file.

---
+++ con-master/src/main/java/ba/codecentric/filter/CORSFilter.java
@@ -0,0 +1,35 @@
+package ba.codecentric.filter;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Value("${cors.allowed.origins}")
+    private String origin;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", origin);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+
+    @Override
+    public void destroy() {
+
+    }
+}
