3c7d27846bb6d6f6de4cad9506fc60fb3e7e1599
nikola <nikola.lisicic@hotmail.com>
2017-04-06 17:50:59 +0200

    CON-15.3 - Add Room model

---
+++ con-base/src/main/java/ba/codecentric/base/domain/Room.java
@@ -0,0 +1,98 @@
+package ba.codecentric.base.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    @Column(name = "number_of_people")
+    private int numberOfPeople;
+
+    @Column(name = "number_of_kids")
+    private int numberOfKids;
+
+    private int size;
+
+    private String image;
+
+    @OneToOne
+    @JoinColumn(name = "bed_id")
+    private Bed bed;
+
+    @ManyToMany
+    @JoinTable(name = "room_feature", joinColumns = {@JoinColumn(name = "room_id")}, inverseJoinColumns = {
+            @JoinColumn(name = "feature_id")})
+    private List<Feature> features = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public int getNumberOfKids() {
+        return numberOfKids;
+    }
+
+    public void setNumberOfKids(int numberOfKids) {
+        this.numberOfKids = numberOfKids;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Bed getBed() {
+        return bed;
+    }
+
+    public void setBed(Bed bed) {
+        this.bed = bed;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+}
