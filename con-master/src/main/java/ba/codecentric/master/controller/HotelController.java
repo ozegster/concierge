7f8011e27053ddd091baed6f13c305a72b5d8f31
DraganKec <Omen1983>
2017-04-06 18:07:37 +0200

    CON-16 Add Validator to controller and return validation message

--- con-master/src/main/java/ba/codecentric/master/controller/HotelController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/HotelController.java
@@ -8,18 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
 @RestController
 public class HotelController {
 
     private final HotelService hotelService;
 
+    private final Validator validator;
+
     @Autowired
-    public HotelController(HotelService hotelService) {
+    public HotelController(HotelService hotelService, Validator validator) {
         this.hotelService = hotelService;
+        this.validator = validator;
     }
 
     @RequestMapping(value = "/hotel", method = RequestMethod.POST)
-    public Hotel saveHotel(@RequestBody Hotel hotel) {
+    public Hotel saveHotel(@RequestBody Hotel hotel) throws Exception {
+
+        Set<ConstraintViolation<Hotel>> constraintViolations = validator.validate(hotel);
+
+        if (constraintViolations.size() > 0)
+            throw new Exception(constraintViolations.iterator().next().getMessage());
+
         return hotelService.saveHotel(hotel);
 
     }
