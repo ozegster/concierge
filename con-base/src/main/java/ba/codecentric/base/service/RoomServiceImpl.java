8c47a035ad847b3a2c2ae888081d4dee2929669d
nikola <nikola.lisicic@hotmail.com>
2017-04-06 17:57:46 +0200

    CON-15.3 - Add RoomService and RoomServiceImpl

---
+++ con-base/src/main/java/ba/codecentric/base/service/RoomServiceImpl.java
@@ -0,0 +1,22 @@
+package ba.codecentric.base.service;
+
+import ba.codecentric.base.domain.Room;
+import ba.codecentric.base.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomServiceImpl implements RoomService {
+
+    private final RoomRepository roomRepository;
+
+    @Autowired
+    public RoomServiceImpl(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public Room saveRoom(Room room) {
+        return roomRepository.save(room);
+    }
+}
