6a609cd12c9296a89143f52c8e27a9fa4ba8d9da
BojanJakic <bojanjakic1@gmail.com>
2017-04-06 14:21:08 +0200

    CON-15.1 Create Feature class

---
+++ con-base/src/main/java/ba/codecentric/base/domain/Feature.java
@@ -0,0 +1,37 @@
+package ba.codecentric.base.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "feature")
+public class Feature {
+
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Id: " + this.getId() + " ,Name: " + this.getName();
+    }
+}
