8cbd25eaebc3775547f898ca22f73e49d8f1ad1f
BojanJakic <bojanjakic1@gmail.com>
2017-04-10 22:19:32 +0200

    CON-15.4 Inject FeatureService, add getFeatures() function

--- dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js
+++ dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js
@@ -3,11 +3,18 @@
     angular.module('ConciergeApp.pages.roomType')
         .controller('RoomTypeCtrl', RoomTypeCtrl);
 
-    RoomTypeCtrl.$inject = ['BedTypeService', '$scope'];
-    function RoomTypeCtrl(BedTypeService, $scope) {
+    RoomTypeCtrl.$inject = ['BedTypeService', 'FeatureService', '$scope'];
+    function RoomTypeCtrl(BedTypeService, FeatureService, $scope) {
 
         BedTypeService.getBedTypes().then(function (response) {
             $scope.beds = response.data;
+        }, function (error) {
+            console.log(error);
+        });
+
+        FeatureService.getFeatures().then(function (response) {
+            $scope.features = response.data;
+            console.log($scope.features)
         }, function (error) {
             console.log(error);
         });
