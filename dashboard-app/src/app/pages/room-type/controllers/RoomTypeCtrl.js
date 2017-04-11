1731893cecb4834842f875e05f17027b46f3c18c
BojanJakic <bojanjakic1@gmail.com>
2017-04-11 18:50:52 +0200

    Merge branch 'CON-15-As_admin_user_I_want_page_to_add_room_types' of https://github.com/ozegster/concierge into CON-15-As_admin_user_I_want_page_to_add_room_types
    # Conflicts:
    #	dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js

--- dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js
+++ dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js
@@ -7,9 +7,6 @@
     function RoomTypeCtrl(BedTypeService, FeatureService, RoomTypeService, $scope) {
 
         $scope.roomType = {};
-
-
-        $scope.roomType = {};
         $scope.selected = [];
 
         BedTypeService.getBedTypes().then(function (response) {
@@ -19,29 +16,26 @@
         });
 
         FeatureService.getFeatures().then(function (response) {
-            $scope.features = response.data;
+            $scope.listOfFeatures = response.data;
 
         }, function (error) {
             console.log(error);
         });
 
        $scope.toggleSelection = function (feature) {
+           var index = $scope.selected.indexOf(feature);
 
-         var index = $scope.selected.indexOf(feature);
-         if (index === -1) {
-            $scope.selected.push(feature);
-
-         } else {
+           if (index === -1) {
+              $scope.selected.push(feature);
+           } else {
             $scope.selected.splice(index,1);
-         }
-
-         $scope.roomType.list = $scope.selected;
-
-                   }
+           }
+            $scope.roomType.features = $scope.selected;
+       }
 
         $scope.submit = function () {
-            console.log($scope.roomType,$scope.roomType.image);
-            RoomTypeService.saveRoomType($scope.roomType,$scope.roomType.image).then(function (response) {
+
+            RoomTypeService.saveRoomType($scope.roomType,$scope.image).then(function (response) {
                 console.log(response);
             }, function (error) {
                 console.log(error);
