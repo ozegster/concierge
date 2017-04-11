62cb65a1b2ee7e81af6bfd7ed0a916d89fb54bd4
BojanJakic <bojanjakic1@gmail.com>
2017-04-11 12:56:25 +0200

    CON-15.4 Add toggleSelection() function that creates list of checked features

--- dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js
+++ dashboard-app/src/app/pages/room-type/controllers/RoomTypeCtrl.js
@@ -3,8 +3,12 @@
     angular.module('ConciergeApp.pages.roomType')
         .controller('RoomTypeCtrl', RoomTypeCtrl);
 
+
     RoomTypeCtrl.$inject = ['BedTypeService', 'FeatureService', '$scope'];
     function RoomTypeCtrl(BedTypeService, FeatureService, $scope) {
+
+        $scope.roomType = {};
+        $scope.selected = [];
 
         BedTypeService.getBedTypes().then(function (response) {
             $scope.beds = response.data;
@@ -14,9 +18,23 @@
 
         FeatureService.getFeatures().then(function (response) {
             $scope.features = response.data;
-            console.log($scope.features)
+
         }, function (error) {
             console.log(error);
         });
+
+       $scope.toggleSelection = function (feature) {
+
+         var index = $scope.selected.indexOf(feature);
+         if (index === -1) {
+            $scope.selected.push(feature);
+
+         } else {
+            $scope.selected.splice(index,1);
+         }
+
+         $scope.roomType.list = $scope.selected;
+
+                   }
     }
 })();
