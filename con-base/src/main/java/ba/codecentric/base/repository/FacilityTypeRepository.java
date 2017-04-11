c02619b6fc79bce35bac73057ce706bc91720883
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-04-11 18:29:16 +0200

    CON - 17.1 Delete Facility in FacilityTypeRepository

--- con-base/src/main/java/ba/codecentric/base/repository/FacilityTypeRepository.java
+++ con-base/src/main/java/ba/codecentric/base/repository/FacilityTypeRepository.java
@@ -1,11 +1,9 @@
 package ba.codecentric.base.repository;
 
-import ba.codecentric.base.domain.Facility;
 import ba.codecentric.base.domain.FacilityType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface FacilityTypeRepository extends JpaRepository<FacilityType, Integer> {
 
-    Facility findFirstByOrderById();
 }
