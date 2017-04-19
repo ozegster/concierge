cff07b7e126fe2542584fb1b49c7f365f120922b
DraganKec <Omen1983>
2017-04-19 18:03:15 +0200

    CON-17 Add 'MediaType.MULTIPART_FORM_DATA_VALUE' to controller and resolve conflict

--- con-master/src/main/java/ba/codecentric/master/controller/FacilityController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/FacilityController.java
@@ -4,11 +4,14 @@ import ba.codecentric.base.domain.Facility;
 import ba.codecentric.base.service.FacilityService;
 import ba.codecentric.base.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 @RestController
 public class FacilityController {
@@ -22,8 +25,8 @@ public class FacilityController {
         this.imageService = imageService;
     }
 
-    @RequestMapping(value = "/facility", method = RequestMethod.POST, consumes = {"multipart/form-data"})
-    public Facility saveFacility(@RequestPart("image") MultipartFile image, @RequestPart("facility") Facility facility) {
+    @RequestMapping(value = "/facility", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Facility saveFacility(@RequestPart("image") MultipartFile image, @Valid @RequestPart("facility") Facility facility) {
         facility.setImage(imageService.saveImage(image));
         return facilityService.saveFacility(facility);
     }
