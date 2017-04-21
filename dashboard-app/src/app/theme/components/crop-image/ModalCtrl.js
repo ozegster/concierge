252ccf0bff4c0fec40909a579c3bf13100c6b104
BojanJakic <bojanjakic1@gmail.com>
2017-04-21 14:09:59 +0200

    CON-18 Add getFileFromCroppedImage(), refactor code

--- dashboard-app/src/app/theme/components/crop-image/ModalCtrl.js
+++ dashboard-app/src/app/theme/components/crop-image/ModalCtrl.js
@@ -4,19 +4,21 @@
     angular.module('ConciergeApp.theme')
         .controller('ModalCtrl', ModalCtrl);
 
-    function ModalCtrl ($scope,$timeout,$uibModalInstance) {
+    function ModalCtrl ($scope,$timeout,$uibModalInstance,$rootScope) {
         $scope.cropper = {};
 
         $scope.getFileSystem = function () {
-           $timeout (function () {
-               var fileInput = document.getElementById('upload-image');
-               fileInput.click();
-           });
+             var fileInput = document.getElementById('upload-image');
+             fileInput.click();
         }
 
         $scope.getCroppedImage = function () {
+            if($scope.cropper.croppedImage == null){
+               return;
+            }
             $scope.imageSrc = angular.element(document.querySelector('#room-type-image'));
             $scope.imageSrc.attr('src',$scope.cropper.croppedImage);
+            $scope.getFileFromCroppedImage();
             $uibModalInstance.dismiss();
         }
 
@@ -24,5 +26,13 @@
             $uibModalInstance.dismiss();
         }
 
+        $scope.getFileFromCroppedImage = function () {
+            var selectedImg = document.querySelector('input[type=file]').files[0];
+            var reader = new FileReader();
+            $rootScope.croppedImg = new File([$scope.cropper.croppedImage],selectedImg.name);
+            if ($rootScope.croppedImg) {
+                reader.readAsDataURL($rootScope.croppedImg);
+            }
+        }
     }
 })()
