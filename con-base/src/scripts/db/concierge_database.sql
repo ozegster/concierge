645929aa28635bf07e5bdbb93ec929a0a7064870
nikola <nikola.lisicic@hotmail.com>
2017-04-25 12:46:38 +0200

    CON-22.1 - Create room table script and update concierge_database.sql

--- con-base/src/scripts/db/concierge_database.sql
+++ con-base/src/scripts/db/concierge_database.sql
@@ -118,3 +118,12 @@ CREATE TABLE `concierge`.`room_type_feature` (
   FOREIGN KEY (`room_type_id`) REFERENCES `concierge`.`room_type` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   FOREIGN KEY (`feature_id`) REFERENCES `concierge`.`feature` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `concierge`.`room` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(4) NOT NULL,
+  `floor_number` tinyint(4) NOT NULL,
+  `room_type_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`room_type_id`) REFERENCES `concierge`.`room_type` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
