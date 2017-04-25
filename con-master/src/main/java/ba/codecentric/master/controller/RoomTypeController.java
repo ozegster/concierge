5af098b5380030be425c23ff51f3dc6037e8f758
nikola <nikola.lisicic@hotmail.com>
2017-04-25 22:34:14 +0200

    CON-22.3 - Implement image sending in RoomTypeController

--- con-master/src/main/java/ba/codecentric/master/controller/RoomTypeController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/RoomTypeController.java
@@ -4,16 +4,25 @@ package ba.codecentric.master.controller;
 import ba.codecentric.base.domain.RoomType;
 import ba.codecentric.base.service.ImageService;
 import ba.codecentric.base.service.RoomTypeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import sun.nio.ch.IOUtil;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class RoomTypeController {
@@ -35,7 +44,15 @@ public class RoomTypeController {
             return roomTypeService.saveRoom(roomType);
         }
         return new RoomType();
+    }
 
+    @RequestMapping(value = "/room-types", method = RequestMethod.GET)
+    public List<RoomType> getRoomTypes() {
+        return roomTypeService.getAllRoomTypes();
+    }
 
+    @RequestMapping(value = "/room-type/image", method = RequestMethod.GET, produces = {"image/jpg", "image/jpeg", "image/png"})
+    public ResponseEntity<InputStreamResource> getImage(@RequestParam String imageName) throws IOException {
+        return new ResponseEntity<InputStreamResource>(new InputStreamResource(imageService.loadImage(imageName)), HttpStatus.OK);
     }
 }
