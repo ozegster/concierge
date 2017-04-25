935bd0f3352bf97e527e1560c2013ce3fcc76f5f
nikola <nikola.lisicic@hotmail.com>
2017-04-25 12:50:20 +0200

    CON-22.1 - Add Room class

---
+++ con-base/src/main/java/ba/codecentric/base/domain/Room.java
@@ -0,0 +1,55 @@
+package ba.codecentric.base.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Integer number;
+
+    private Integer floorNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "room_type_id")
+    private RoomType roomType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getFloorNumber() {
+        return floorNumber;
+    }
+
+    public void setFloorNumber(Integer floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+}
