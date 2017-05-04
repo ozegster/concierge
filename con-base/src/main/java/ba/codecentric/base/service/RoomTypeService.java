a62d85552d83fd2ce69095f19f97f38dc7d1c5cf
BojanJakic <bojanjakic1@gmail.com>
2017-05-04 17:03:57 +0200

    CON-18 Reformat code

--- con-base/src/main/java/ba/codecentric/base/service/RoomTypeService.java
+++ con-base/src/main/java/ba/codecentric/base/service/RoomTypeService.java
@@ -2,8 +2,11 @@ package ba.codecentric.base.service;
 
 
 import ba.codecentric.base.domain.RoomType;
+import java.util.List;
 
 public interface RoomTypeService {
 
     RoomType saveRoom(RoomType roomType);
+    List<RoomType> getAllRoomTypes();
+    void deleteRoomType(RoomType roomType);
 }
