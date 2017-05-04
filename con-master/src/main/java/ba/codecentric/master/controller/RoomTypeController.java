ee1c182f7ff14c70159f688a8976c349ecde31f4
BojanJakic <bojanjakic1@gmail.com>
2017-05-04 17:10:27 +0200

    CON-18 Add following methods : getRoomType(), getDirectoryPath(), deleteRoomType(@RequestBody RoomType roomType)

--- con-master/src/main/java/ba/codecentric/master/controller/RoomTypeController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/RoomTypeController.java
@@ -5,21 +5,22 @@ import ba.codecentric.base.domain.RoomType;
 import ba.codecentric.base.service.ImageService;
 import ba.codecentric.base.service.RoomTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class RoomTypeController {
 
     private final RoomTypeService roomTypeService;
     private final ImageService imageService;
+
+    @Value ("${image.directory.path}")
+    String directoryPath;
 
     @Autowired
     public RoomTypeController(RoomTypeService roomTypeService, ImageService imageService) {
@@ -35,7 +36,21 @@ public class RoomTypeController {
             return roomTypeService.saveRoom(roomType);
         }
         return new RoomType();
+    }
 
+    @RequestMapping(value = "/room-type", method = RequestMethod.GET)
+    public List<RoomType> getRoomType(){
+        return roomTypeService.getAllRoomTypes();
+    }
 
+    @RequestMapping(value = "/directory" ,method = RequestMethod.GET, produces="text/plain")
+    public String getDirectoryPath(){
+        return directoryPath;
+    }
+
+    @RequestMapping(value = "/delete-room-type", method = RequestMethod.POST)
+    public RoomType deleteRoomType(@RequestBody RoomType roomType){
+        roomTypeService.deleteRoomType(roomType);
+        return roomType;
     }
 }
