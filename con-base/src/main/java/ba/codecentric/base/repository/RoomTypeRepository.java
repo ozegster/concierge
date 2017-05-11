cd0a8be2f53ba47277e0e65711073fc1a0f7e98f
BojanJakic <bojanjakic1@gmail.com>
2017-05-11 21:49:50 +0200

    Con-21 Add findById(Integer id) method

--- con-base/src/main/java/ba/codecentric/base/repository/RoomTypeRepository.java
+++ con-base/src/main/java/ba/codecentric/base/repository/RoomTypeRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface RoomTypeRepository extends JpaRepository<RoomType, Integer> {
+
+    void deleteById(Integer roomTypeId);
 }
