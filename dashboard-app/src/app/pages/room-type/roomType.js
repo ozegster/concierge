92bde831e124af491e8bb24d646699083fbb4667
BojanJakic <bojanjakic1@gmail.com>
2017-05-15 18:24:44 +0200

    Con-21 Refactor code, delete unused code

--- dashboard-app/src/app/pages/room-type/roomType.js
+++ dashboard-app/src/app/pages/room-type/roomType.js
@@ -7,39 +7,6 @@
 
 
     /** @ngInject */
-    function routeConfig($stateProvider) {
-        $stateProvider
-            .state('room', {
-                url: '/room',
-                template: '<ui-view autoscroll="true" autoscroll-body-top></ui-view>',
-                title: 'Room',
-                sidebarMeta: {
-                    icon: 'ion-compose',
-                    order: 250,
-                },
-            })
-            .state('room.roomType', {
-                url: '/roomType',
-                templateUrl: 'app/pages/room-type/views/room-type.html',
-                title: 'Room Type',
-                sidebarMeta: {
-                    order: 0,
-                },
-                controller: 'RoomTypeCtrl'
-            })
-            .state('room.room', {
-                url: '/newRoom',
-                templateUrl: 'app/pages/room-type/views/room.html',
-                title: 'Room',
-                sidebarMeta: {
-                    order: 0,
-                },
-                controller: 'RoomCtrl'
-            })
-    }
-
-
-  /** @ngInject */
   function routeConfig($stateProvider,baSidebarServiceProvider) {
        $stateProvider
            .state('room', {
@@ -59,7 +26,6 @@
                      order: 0,
                   },
                   controller:'RoomTypeCtrl'
-
            })
            .state('room.roomType', {
                   url: '/roomType',
@@ -67,7 +33,15 @@
                   title: 'New Room Type',
                   controller:'RoomTypeCtrl'
            })
+           .state('room.room', {
+               url: '/newRoom',
+               templateUrl: 'app/pages/room-type/views/room.html',
+               title: 'Room',
+               sidebarMeta: {
+                   order: 0,
+               },
+               controller: 'RoomCtrl'
+           })
   }
-
 })();
 
