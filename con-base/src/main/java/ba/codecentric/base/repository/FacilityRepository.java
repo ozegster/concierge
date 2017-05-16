38a6abb5979d7e2287b64d694a19a7a14d85a740
BojanJakic <bojanjakic1@gmail.com>
2017-05-16 16:33:20 +0200

    #32-Add existsByFacilityName(String name) method

--- con-base/src/main/java/ba/codecentric/base/repository/FacilityRepository.java
+++ con-base/src/main/java/ba/codecentric/base/repository/FacilityRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FacilityRepository extends JpaRepository<Facility, Integer> {
 
+    boolean existsByFacilityName(String name);
+
 }
