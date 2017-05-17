e2bf8cbb679a44bbcc46409c74e0dfcd2833f606
nikola <nikola.lisicic@hotmail.com>
2017-05-17 21:54:13 +0200

    #34 - Add logging for RoomController

--- con-master/src/main/java/ba/codecentric/master/controller/RoomController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/RoomController.java
@@ -2,6 +2,7 @@ package ba.codecentric.master.controller;
 
 import ba.codecentric.base.domain.Room;
 import ba.codecentric.base.service.RoomService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import java.util.List;
 public class RoomController {
 
     private final RoomService roomService;
+    private final static Logger LOG = Logger.getLogger(RoomController.class);
 
     @Autowired
     public RoomController(RoomService roomService) {
@@ -26,6 +28,7 @@ public class RoomController {
     @PostMapping(value = "/rooms")
     public void save(@Valid @RequestBody Room room) {
         roomService.saveRoom(room);
+        LOG.info("Save Room with number: " + room.getNumber());
     }
 
     @GetMapping(value = "/rooms")
@@ -33,8 +36,9 @@ public class RoomController {
         return roomService.getAllRooms();
     }
 
-    @DeleteMapping(value="/rooms/{id}")
-    public void delete(@PathVariable Integer id){
+    @DeleteMapping(value = "/rooms/{id}")
+    public void delete(@PathVariable Integer id) {
         roomService.deleteRoom(id);
+        LOG.info("Delete Room with id: " + id);
     }
 }
