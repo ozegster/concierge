a519127ae1b43c1d39cfd0fca65aaac9767e4037
nikola <nikola.lisicic@hotmail.com>
2017-05-17 20:49:18 +0200

    #34 - Add mapping for deleting room

--- con-master/src/main/java/ba/codecentric/master/controller/RoomController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/RoomController.java
@@ -3,7 +3,9 @@ package ba.codecentric.master.controller;
 import ba.codecentric.base.domain.Room;
 import ba.codecentric.base.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,10 @@ public class RoomController {
     @GetMapping(value = "/rooms")
     public List<Room> getRooms() {
         return roomService.getAllRooms();
+    }
+
+    @DeleteMapping(value="/rooms/{id}")
+    public void delete(@PathVariable Integer id){
+        roomService.deleteRoom(id);
     }
 }
