0cf584b3233dd7ff29d287ea32e3baaaa9e7052e
BojanJakic <bojanjakic1@gmail.com>
2017-05-22 11:03:27 +0200

    #32 Do not use wildcard imports

--- con-master/src/main/java/ba/codecentric/master/controller/FacilityController.java
+++ con-master/src/main/java/ba/codecentric/master/controller/FacilityController.java
@@ -5,9 +5,12 @@ import ba.codecentric.base.service.FacilityService;
 import ba.codecentric.base.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.Valid;
 import java.io.IOException;
 
