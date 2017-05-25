3d2629b2c57e979552f42010216ea184741eb0e4
DraganKec <Omen1983>
2017-05-25 20:49:31 +0200

    #37 Create schedule for resetting password to 0

--- con-base/src/main/java/ba/codecentric/base/repository/RoomCheckInRepository.java
+++ con-base/src/main/java/ba/codecentric/base/repository/RoomCheckInRepository.java
@@ -2,8 +2,15 @@ package ba.codecentric.base.repository;
 
 import ba.codecentric.base.domain.RoomCheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+
 
 public interface RoomCheckInRepository extends JpaRepository<RoomCheckIn, Integer> {
 
     RoomCheckIn findByPassword(Integer password);
+
+    @Query(value = "SELECT r FROM RoomCheckIn r WHERE r.checkOut<?1 AND r.password>0")
+    Iterable<RoomCheckIn> findByCheckOutDate(Date checkOut);
 }
