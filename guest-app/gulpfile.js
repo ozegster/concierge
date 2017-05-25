a3e1fbf6de293c73d7b9f4dd0b5a9b8f633d4807
BojanJakic <bojanjakic1@gmail.com>
2017-05-25 19:16:33 +0200

    #43 Create gulp task(guest-app-home) that open index.html in browser

--- guest-app/gulpfile.js
+++ guest-app/gulpfile.js
@@ -1,1 +1,12 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
+gulp.task('guest-app-home', function () {
+    gulp.src('src')
+        .pipe(webserver({
+            fallback:'src/index.html',
+            open:true,
+            port: 3000,
+            livereload: true
+        }));
+});
