35a29eda6b9c79ab8f74b3f388d0d1fc50d5bbc0
DraganKec <Omen1983>
2017-05-26 16:54:01 +0200

    #37 Change checkIn to roomCheckIn

--- dashboard-app/src/app/pages/room-check-in/controllers/CheckInCtrl.js
+++ dashboard-app/src/app/pages/room-check-in/controllers/CheckInCtrl.js
@@ -7,24 +7,21 @@
     CheckInCtrl.$inject = ['$scope', 'toastr', 'CheckInService', '$uibModal'];
     function CheckInCtrl($scope, toastr, CheckInService, $uibModal) {
 
-        $scope.checkIn = {};
+        $scope.roomCheckIn = {};
 
-        $scope.getAvailableRooms = function (checkIn) {
-            CheckInService.getAvailableRooms(checkIn).then(function (response) {
+        $scope.getAvailableRooms = function () {
+            CheckInService.getAvailableRooms($scope.roomCheckIn).then(function (response) {
                 $scope.availableRooms = response.data;
                 $scope.rowCollection = response.data;
             })
         };
 
-        $scope.submit = function (roomCheckIn) {
-            console.log($scope.checkIn);
-            if (roomCheckIn.$invalid) {
+        $scope.submit = function (roomCheckInForm) {
+            if (roomCheckInForm.$invalid) {
                 return;
             }
-            CheckInService.saveCheckIn($scope.checkIn).then(function (response) {
-
+            CheckInService.saveCheckIn($scope.roomCheckIn).then(function (response) {
                 if (response.status === 200) {
-                    // toastr.success(response.data.name + ' has successfully saved', 'Save check in');
                     $scope.message = response.data.name + ' has successfully saved, password:';
                     $scope.bigMessage = response.data.password;
                     $uibModal.open({
@@ -32,26 +29,14 @@
                         controller: 'CheckInCtrl',
                         scope: $scope
                     });
-                    // roomCheckIn.$setPristine();
-                    // roomCheckIn.$setUntouched();
-                    // $scope.checkIn = {};
+                    roomCheckInForm.$setPristine();
+                    roomCheckInForm.$setUntouched();
+                    $scope.roomCheckIn = {};
                 } else {
                     toastr.error(response.data.error, 'Error');
                 }
             }, function (error) {
                 console.log(error);
-            });
-        };
-        $scope.openaaaa = function (page, size) {
-            $uibModal.open({
-                animation: true,
-                templateUrl: page,
-                size: size,
-                resolve: {
-                    items: function () {
-                        return $scope.items;
-                    }
-                }
             });
         };
     }
