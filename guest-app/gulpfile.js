2beea70abc20c3f0e6f0436cd12fd49ee4247445
BojanJakic <bojanjakic1@gmail.com>
2017-06-05 13:24:19 +0200

    #44 Keep only serve task in this file

--- guest-app/gulpfile.js
+++ guest-app/gulpfile.js
@@ -1,32 +1,8 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
-var inject = require('gulp-inject');
-var mainBowerFiles = require('main-bower-files');
 var runSequence = require('run-sequence');
 
 gulp.task('serve', function () {
-    gulp.src('.tmp')
-        .pipe(webserver({
-            fallback:'./serve/index.html',
-            open:true,
-            livereload: true,
-            port: 8000
-        }));
+    runSequence('clean','style','inject','openBrowser')
 });
 
-var paths = {
-    javascript : 'src/app/**/*.js',
-    css : ''
-};
-
-gulp.task('inject', function () {
-    return gulp.src('./src/index.html')
-        .pipe(inject(gulp.src(mainBowerFiles(), {read: false}), {name: 'bower', relative: true}))
-        .pipe(inject(gulp.src(paths.javascript,{read:false})))
-        .pipe(gulp.dest('./gulp'));
-});
-
-
-gulp.task('start', function () {
-    runSequence('inject','serve')
-});
+require('require-dir')('./gulp');
