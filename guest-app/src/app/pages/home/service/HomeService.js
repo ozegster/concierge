9cda58489d76ed62a78c783e6db3d59485b2f605
BojanJakic <bojanjakic1@gmail.com>
2017-06-05 13:18:23 +0200

    #44 Create HomeService, add getHotel() function

---
+++ guest-app/src/app/pages/home/service/HomeService.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular.module('GuestApp.pages.home')
+        .factory('HomeService', ['$http', '$q', function ($http,$q) {
+
+            var getHotel = function () {
+                var deferred = $q.defer();
+                $http.get(location.protocol + '//' + location.hostname + ':8090' + '/hotels')
+                    .then(function (response) {
+                        if (!response.data) {
+                            deferred.reject("There isn't a hotel in db");
+                        } else {
+                            deferred.resolve(response);
+                        }
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+                return deferred.promise;
+            };
+
+        return {
+            getHotel : getHotel
+        };
+        }])
+})();
