2855889a614de21e69a722ad7854cf9df71d6581
BojanJakic <bojanjakic1@gmail.com>
2017-06-08 14:34:35 +0200

    #44 Add getLogo(logoName) function

--- guest-app/src/app/pages/home/service/HomeService.js
+++ guest-app/src/app/pages/home/service/HomeService.js
@@ -19,8 +19,31 @@
                 return deferred.promise;
             };
 
+            var getLogo = function (logoName) {
+                return $http({
+                    method: 'GET',
+                    url: location.protocol + '//' + location.hostname + ':8090' + '/hotel/imageLogo/' + logoName,
+                    responseType: "arraybuffer"
+                }).then(function (response) {
+                    return arrayBufferToBase64(response.data);
+                }, function (error) {
+                    return error;
+                });
+            };
+
+            function arrayBufferToBase64(buffer) {
+                var binary = '';
+                var bytes = new Uint8Array(buffer);
+                var len = bytes.byteLength;
+                for (var i = 0; i < len; i++) {
+                    binary += String.fromCharCode(bytes[i]);
+                }
+                return window.btoa(binary);
+            }
+
         return {
-            getHotel : getHotel
+            getHotel : getHotel,
+            getLogo : getLogo
         };
         }])
 })();
