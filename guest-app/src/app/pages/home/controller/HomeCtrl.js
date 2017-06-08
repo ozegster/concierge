0520c03c4ea2c8fd96f223dbc6639b425e1c22a9
BojanJakic <bojanjakic1@gmail.com>
2017-06-08 14:34:03 +0200

    #44 Add $scope.hotel object in code, make $scope.getImage(logoName) function

--- guest-app/src/app/pages/home/controller/HomeCtrl.js
+++ guest-app/src/app/pages/home/controller/HomeCtrl.js
@@ -6,10 +6,27 @@
     HomeCtrl.$inject = ['$scope','HomeService'];
     function HomeCtrl($scope, HomeService) {
 
+        $scope.hotel = {};
+
         HomeService.getHotel().then(function (response) {
-            $scope.hotelName = response.data.name;
+            if(response.data) {
+                $scope.hotel = response.data;
+                $scope.getImage($scope.hotel.imageLogo);
+                $scope.hotel.name = response.data.name;
+            } else {
+                console.log('Hotel not found')
+            }
         }, function () {
-            $scope.hotelName = 'Not available';
+            $scope.hotel.name = 'Not available';
         });
+
+        $scope.getImage = function (logoName) {
+            HomeService.getLogo(logoName).then(function (data) {
+                var logo = angular.element(document.querySelector('.logo-img'));
+                logo.attr('src', 'data:image/jpeg;base64,' + data);
+            }, function (error) {
+                console.log(error);
+            });
+        }
     }
 })();
