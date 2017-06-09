05acaeb268d6c6bfca88d9a0c34d96195e129979
BojanJakic <bojanjakic1@gmail.com>
2017-06-09 13:41:56 +0200

    #44 Adapt code to accept multiple different domains, change String origin -> String[ ] origin

--- con-master/src/main/java/ba/codecentric/master/filter/CORSFilter.java
+++ con-master/src/main/java/ba/codecentric/master/filter/CORSFilter.java
@@ -7,17 +7,21 @@ import org.springframework.stereotype.Component;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class CORSFilter implements Filter {
 
     @Value("${cors.allowed.origins}")
-    private String origin;
+    private String[] origin;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", origin);
+
+        for(String domain : origin) {
+            response.setHeader("Access-Control-Allow-Origin", domain);
+        }
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         chain.doFilter(req, res);
