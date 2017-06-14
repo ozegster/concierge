367d2f634b1cce75d2c79ff4123a8825d4aab377
nikola <nikola.lisicic@hotmail.com>
2017-06-14 13:38:14 +0200

    Fix CORS filter to allow both dashboard and guest app origins

--- con-master/src/main/java/ba/codecentric/master/filter/CORSFilter.java
+++ con-master/src/main/java/ba/codecentric/master/filter/CORSFilter.java
@@ -4,9 +4,16 @@ package ba.codecentric.master.filter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -18,12 +25,15 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
 
-        for(String domain : origin) {
-            response.setHeader("Access-Control-Allow-Origin", domain);
+        List<String> allowedOrigins = Arrays.asList(origin);
+        String requestOriginHeader = request.getHeader("Origin");
+        if (allowedOrigins.contains(requestOriginHeader)) {
+            response.setHeader("Access-Control-Allow-Origin", requestOriginHeader);
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         }
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         chain.doFilter(req, res);
     }
 
