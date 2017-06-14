3d4c46fff10f835d4929c24cdd69756a502193bc
nikola <nikola.lisicic@hotmail.com>
2017-06-14 18:05:50 +0200

    #47 - Implement getHotel method

--- guest-app/src/app/pages/hotel-info/controller/HotelInfoCtrl.js
+++ guest-app/src/app/pages/hotel-info/controller/HotelInfoCtrl.js
@@ -3,8 +3,21 @@
 
     angular.module('GuestApp.pages.hotelInfo').controller('HotelInfoCtrl', HotelInfoCtrl);
 
-    function HotelInfoCtrl() {
+    HotelInfoCtrl.$inject = ['$scope', 'HomeService'];
+    function HotelInfoCtrl($scope, HomeService) {
 
+        $scope.hotel = {};
+
+        HomeService.getHotel().then(function (response) {
+            if (response.data) {
+                $scope.hotel = response.data;
+                $scope.hotel.name = response.data.name;
+            } else {
+                console.log('Hotel not found')
+            }
+        }, function (error) {
+            console.log(error);
+        });
     };
 
 })();
