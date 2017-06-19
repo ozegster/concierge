a763758de9a13d622574b677ff946bf1515c4a8d
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-06-19 02:22:20 +0200

    # 54 Refactor code

--- guest-app/src/app/pages/facilities/controller/FacilitiesCtrl.js
+++ guest-app/src/app/pages/facilities/controller/FacilitiesCtrl.js
@@ -7,21 +7,11 @@
     function FacilitiesCtrl($scope, FacilitiesService, HomeService) {
 
         $scope.message = '';
-        $scope.selectedFacility = false;
+        $scope.selected = false;
 
-        FacilitiesService.getAllFacilities().then(function (response) {
-            if(response.data) {
-                $scope.listOfFacilities = response.data;
-                var image = angular.element(document.querySelector('.facility-image'));
-            } else {
-                $scope.message = 'not available';
-            }
-        }, function (error) {
-            console.log(error);
-        });
 
         HomeService.getHotel().then(function (response) {
-            if(response.data) {
+            if (response.data) {
                 $scope.hotel = response.data;
                 var logo = angular.element(document.querySelector('.logo-img'));
                 logo.attr('src', $scope.hotel.imageLogo);
@@ -32,8 +22,25 @@
             console.log(error);
         });
 
-        $scope.selectedFacilityShow = function () {
-            $scope.selectedFacility = !$scope.selectedFacility;
-        }
+        FacilitiesService.getAllFacilities().then(function (response) {
+            if (response.data) {
+                $scope.listOfFacilities = response.data;
+                var image = angular.element(document.querySelector('.facility-image'));
+            } else {
+                $scope.message = 'not available';
+            }
+        }, function (error) {
+            console.log(error);
+        });
+
+        $scope.selectedDetails = function () {
+            $scope.selected = !$scope.selected;
+        };
+
+        $scope.getMoreDetails = function (facilityName) {
+             $scope.selectedFacility = facilityName;
+        };
+
+
     }
 })();
