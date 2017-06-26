fdc8d12f963a7c1d86253b7571019e10b2579fba
DraganKec <Omen1983>
2017-06-26 20:51:58 +0200

    #37 Finishing with fucking Check in page :)

--- dashboard-app/src/app/pages/room-check-in/controllers/DatepickerCtrl.js
+++ dashboard-app/src/app/pages/room-check-in/controllers/DatepickerCtrl.js
@@ -31,15 +31,6 @@
             var checkInDate = new Date(from[2], from[1] - 1, from[0]);
             $scope.checkOutOpened = true;
             $scope.checkOutOptions.minDate = new Date(checkInDate.setDate(checkInDate.getDate() + 1));
-            //problem je bio u liniji 33, mislim da je to upozorenje koje datapicker direktiva javlja
-            //ti si radio ovo:
-            //$scope.checkOutOptions.minDate = checkInDate.setDate(checkInDate.getDate() + 1);
-            // kad otprintas ovo u konzoli
-            //console.log(typeof  $scope.checkOutOptions.minDate)
-            // rezultat je number, a ono upozorenje je trazilo da se koristi Date objekat
-            //tako da sam samo odradio "wrapping", odnosno
-            //strpao sam $scope.checkOutOptions.minDate unutar new Date() i dobio
-            //Date objekat koji se ocekuje
         }
     }
 })();
