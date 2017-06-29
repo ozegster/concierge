55ba750abc9842ee685735df5d0bc4ae293465c9
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-06-29 02:27:20 +0200

    #61 Refactor code

--- guest-app/gulpfile.js
+++ guest-app/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('serve',['watch'], function () {
-    runSequence('clean','scripts','style','inject','openBrowser')
+    runSequence('clean','style','inject','openBrowser')
 });
 
 require('require-dir')('./gulp');
