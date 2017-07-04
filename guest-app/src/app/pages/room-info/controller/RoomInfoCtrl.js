73738fc3fe4e5c3289b9729b636e23466722af53
DraganKec <Omen1983>
2017-07-04 21:51:22 +0200

    #57 Refactoring html Room info

--- guest-app/src/app/pages/room-info/controller/RoomInfoCtrl.js
+++ guest-app/src/app/pages/room-info/controller/RoomInfoCtrl.js
@@ -3,14 +3,14 @@
 
     angular.module('GuestApp.pages.roomInfo').controller('RoomInfoCtrl', RoomInfoCtrl);
 
-    RoomInfoCtrl.$inject = ['$scope','RoomInfoService'];
+    RoomInfoCtrl.$inject = ['$scope', 'RoomInfoService'];
     function RoomInfoCtrl($scope, RoomInfoService) {
 
         $scope.roomCheckIn = {};
         $scope.air = false;
 
         RoomInfoService.getRoomCheckIn(7368).then(function (response) {
-            if(response.data) {
+            if (response.data) {
                 $scope.roomCheckIn = response.data;
             } else {
                 console.log('Room not found')
@@ -18,5 +18,6 @@
         }, function () {
             $scope.roomCheckIn.room.number = 'Not available';
         });
+
     }
 })();
