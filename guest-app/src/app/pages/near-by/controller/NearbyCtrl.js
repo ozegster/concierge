040de302c4ab0f42759968d75fecb65b6d1862ad
BojanJakic <bojanjakic1@gmail.com>
2017-07-04 21:56:30 +0200

    #48 Add validation logic, check is image available before open modal

--- guest-app/src/app/pages/near-by/controller/NearbyCtrl.js
+++ guest-app/src/app/pages/near-by/controller/NearbyCtrl.js
@@ -2,67 +2,89 @@
     'use strict';
 
     angular.module('GuestApp.pages.nearby').controller('NearbyCtrl', NearbyCtrl);
-    NearbyCtrl.$inject = ['$scope','$window','$uibModal', 'NearbyService'];
+    NearbyCtrl.$inject = ['$scope', '$window', '$uibModal', 'toastr', 'NearbyService'];
 
-    function NearbyCtrl($scope,$window,$uibModal, NearbyService) {
+    function NearbyCtrl($scope, $window, $uibModal, toastr, NearbyService) {
 
         $scope.keyword = "";
+        $scope.radius = "";
         $scope.places = [];
 
-        $scope.getPlaces = function () {
-            NearbyService.getPlaces($scope.keyword).then(function (response) {
+        $scope.getPlaces = function (form) {
+
+            if(form.$invalid){
+                return;
+            }
+            NearbyService.getPlaces($scope.keyword, $scope.radius).then(function (response) {
                 $scope.places = response.data.results;
+                if($scope.places.length === 0){
+                    toastr.info('No Results Found',{
+                        timeOut: 4000,
+                        closeButton: true
+                    });
+                }
             }, function (error) {
                 console.log('error');
             });
         };
 
-        $scope.setMap = function (lat,lng,name) {
+        $scope.setMap = function (lat, lng, name) {
             var myLatLng = {lat: 44.7273374, lng: 18.0773216};
             var placeName = 'Hotel Integra';
 
-            if(lat && lng && name){
+            if (lat && lng && name) {
                 myLatLng.lat = lat;
                 myLatLng.lng = lng;
                 placeName = name;
             }
 
-            var mapProp= {
+            var mapProp = {
                 center: myLatLng,
-                zoom:15,
+                zoom: 15,
             };
 
-            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
             $scope.customizeMarker(myLatLng, map, placeName);
         },
 
             $scope.customizeMarker = function (myLatLng, map, placeName) {
 
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                title: placeName
-            });
+                var marker = new google.maps.Marker({
+                    position: myLatLng,
+                    map: map,
+                    title: placeName
+                });
 
-            $window.scrollTo(0,0)
-        };
+                $window.scrollTo(0, 0)
+            };
 
         $scope.setMap();
 
         $scope.getImage = function (photo) {
-            NearbyService.getImage(photo).then(function(response){
-               $scope.openImageInModal(response.data);
-            },function(error){
-                console.log(error)
-            })
-        }
+
+            if (!photo) {
+                $scope.openImageInModal(photo);
+            } else {
+                NearbyService.getImage(photo).then(function (response) {
+                    $scope.openImageInModal(response.data);
+                }, function (error) {
+                    console.log(error)
+                })
+            }
+        };
 
         $scope.openImageInModal = function (photo) {
-            $scope.placesImage = photo;
+
+            if (!photo) {
+                $scope.placesImage = 'assets/img/noimage.jpg';
+            }else{
+                $scope.placesImage = photo;
+            }
+
             $uibModal.open({
-                templateUrl:'app/pages/near-by/view/image-modal.html',
+                templateUrl: 'app/pages/near-by/view/image-modal.html',
                 controller: 'NearbyCtrl',
-                scope : $scope
+                scope: $scope
             })
 
         }
