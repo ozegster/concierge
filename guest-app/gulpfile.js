1fec49325905506f3e9e30d37868829ac9625e24
BojanJakic <bojanjakic1@gmail.com>
2017-07-05 16:12:07 +0200

    #66 Add watch task in runSequence()

--- guest-app/gulpfile.js
+++ guest-app/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('serve', function () {
-    runSequence('clean','style','inject','openBrowser')
+    runSequence('clean','style','inject','openBrowser','watch')
 });
 
 require('require-dir')('./gulp');
