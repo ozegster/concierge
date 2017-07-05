45002d00d9db4411d0af9e7951ff4f8d00dd6eab
BojanJakic <bojanjakic1@gmail.com>
2017-07-05 16:04:33 +0200

    #66 Inject browserSyncInstanse initialized in server.js, use it's stream() method to inject css changes in browser

--- guest-app/gulp/style.js
+++ guest-app/gulp/style.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var conf = require('./conf');
+var browserSync = require('browser-sync').get('browserSyncInstance');
 
 gulp.task('style', function () {
     return gulp.src(conf.paths.src + conf.paths.css)
         .pipe(sass())
         .pipe(gulp.dest(conf.paths.tmp))
+        .pipe(browserSync.stream());
 });
